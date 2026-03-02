f1dffabb7360d3d7d38a6cbef0b9cfba6a5af8f9
Petar Španja <petar@spanja.info>
2026-03-02 08:52:04 +0100

    NGSTACK-1022: Update build to modern Python environments and latest RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,18 +1,5 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -*- coding: utf-8 -*-
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import sys, os
 import sphinx_rtd_theme
 
@@ -20,12 +7,8 @@ from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
 from pygments.lexers.web import HtmlLexer
 from pygments.lexers.web import JsonLexer
-from datetime import datetime
-# -- Project information -----------------------------------------------------
 
-project = "Netgen's Local Development Setup"
-copyright = '2022, Netgen'
-author = 'Netgen'
+from datetime import datetime
 
 lexers['php'] = PhpLexer(startinline=True)
 lexers['php-annotations'] = PhpLexer(startinline=True)
@@ -33,6 +16,7 @@ lexers['html'] = HtmlLexer(startinline=True)
 lexers['json'] = JsonLexer(startinline=True)
 
 extensions = ['sphinx.ext.autodoc',
+    'sphinx_copybutton',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
@@ -42,20 +26,23 @@ extensions = ['sphinx.ext.autodoc',
 source_suffix = '.rst'
 master_doc = 'index'
 
+project = "Netgen's Local Development Setup"
+copyright = 'Netgen'
+author = 'Netgen'
+
 version = ''
 release = ''
 
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '.virtualenv']
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
 templates_path = ['_templates']
 
 html_theme_options = {
     'collapse_navigation': True,
-    'display_version': True,
+    'version_selector': True,
 }
 
 html_context = {
